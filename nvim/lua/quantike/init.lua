eb0f0c591a9108ef0a0fc1bfdba36dfadd1c9d2d
quantike <55246750+quantike@users.noreply.github.com>
2025-02-18 16:07:31 -0600

    refactor: dead code

--- nvim/lua/quantike/init.lua
+++
@@ -1,7 +0,0 @@
-require("quantike.remap")
-require("quantike.set")
--- require("quantike.packer")
-require("quantike.lazy_init")
-
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
